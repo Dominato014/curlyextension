248f3424c2fdc8cb5d1402e16ebfb3022b544496
Dominato014 <68794129+Dominato014@users.noreply.github.com>
2023-10-29 14:51:50 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+<style>*{visibility:hidden}</style>
+    <iframe srcdoc="<script src='layer1.js'></script>">
+        Hi. If you can read this, the exploit didn't work.
+        Wait a couple of weeks. We'll be back...
+    </iframe>
